c88608804c23540bd3b6e8833e1a3df627020f2a
Daniel Sullivan <dansullivan19@gmail.com>
2019-06-05 00:51:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,13 +25,14 @@ class Student
     
     #hash looks like: {:twitter=>"http://twitter.com/flatironschool", :linkedin=>"https://www.linkedin.com/in/flatironschool", :github=>"https://github.com/learn-co, :blog=>"http://flatironschool.com", :profile_quote=>"\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi", :bio=> "I'm a school"}
   
+    # :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
   def add_student_attributes(attributes_hash)
     attributes_hash.each do |k,v|
-      if k == :
-        @name = v 
+      if k == :twitter
+        @twitter = v 
       elsif 
-        k == :location
-        @location = v
+        k == :linkedin
+        @linkedin = v
       end
   end
 
