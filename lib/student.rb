da2c066eb7e619196dac2cb58bfe5eec0b451363
Daniel Sullivan <dansullivan19@gmail.com>
2019-06-05 00:55:45 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -16,16 +16,44 @@ class Student
     end
   end
 
+    #array looks like => [ {name => "John Doe", location => "Boston, MA", profile_url => "students/johndoe.html"}, {etc....} ]
   def self.create_from_collection(students_array)
-        
+    students_array.each do |e|
+        student = Student.new(e)
+      end
   end
-
-  def add_student_attributes(attributes_hash)
     
+    #hash looks like: {:twitter=>"http://twitter.com/flatironschool", :linkedin=>"https://www.linkedin.com/in/flatironschool", :github=>"https://github.com/learn-co, :blog=>"http://flatironschool.com", :profile_quote=>"\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi", :bio=> "I'm a school"}
+  
+    # :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  def add_student_attributes(attributes_hash)
+    attributes_hash.each do |k,v|
+      if k == :twitter
+        @twitter = v 
+      elsif 
+        k == :linkedin
+        @linkedin = v
+      elsif 
+        k == :github
+        @github = v
+      elsif 
+        k == :blog
+        @blog = v
+      elsif 
+        k == :profile_quote
+        @profile_quote = v
+      elsif 
+        k == :bio
+        @bio = v
+      elsif 
+        k == :profile_url
+        @profile_url = v
+      end
+    end
   end
 
   def self.all
-    
+    @@all
   end
 
 end
